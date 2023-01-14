3c7b31cc192ffa74b9c0eac9fc1051975b13c497
aydin <aydingokce@vt.edu>
2023-01-14 17:14:03 -0500

    fortnite

--- drawing_tool.py
+++ drawing_tool.py
@@ -1,17 +1,18 @@
 from tkinter import Tk, Canvas, Button
 from tkinter.colorchooser import askcolor
-from utils import map_points_to_angles, write_angles_to_servo
-
+from servo_writer import ServoWriter
+import numpy as np
 class DroneCanvas(object):
     DEFAULT_PEN_SIZE = 0.5
     DEFAULT_COLOR = 'black'
     CANVAS_WIDTH = 1000
     CANVAS_HEIGHT = 300
-    PHYSICAL_WIDTH = 300 #mm
-    PHYSICAL_HEIGHT = 100 #mm
+    PHYSICAL_WIDTH = 225 #mm
+    PHYSICAL_HEIGHT = 140 #mm
     
     def __init__(self):
         self.root = Tk()
+        self.writer = ServoWriter()
         
         self.strokes = []
         self.points = []
@@ -20,9 +21,11 @@ class DroneCanvas(object):
         self.burn_button.grid(row=0, column=0)
         self.clear_button = Button(self.root, text='clear', command=self.clear)
         self.clear_button.grid(row=0, column=1)
+        self.toggle_mode_button = Button(self.root, text='toggle mode', command=self.toggle_mode)
+        self.toggle_mode_button.grid(row=0, column=2)
         
         self.c = Canvas(self.root, bg='white', width=self.CANVAS_WIDTH, height=self.CANVAS_HEIGHT)
-        self.c.grid(row=1, columnspan=2, sticky='nsew') 
+        self.c.grid(row=1, columnspan=3, sticky='nsew') 
         
         self.setup()
         self.root.mainloop()
@@ -30,32 +33,71 @@ class DroneCanvas(object):
     def setup(self):
         self.old_x = None
         self.old_y = None
+        self.curr_line_x = None
+        self.curr_line_y = None
+        self.curr_line_obj = None
+        self.draw_mode = "scribble" #line
+        
         self.line_width = self.DEFAULT_PEN_SIZE
         self.color = self.DEFAULT_COLOR
-        self.c.bind('<B1-Motion>', self.paint)
+        self.c.bind('<Button 1>', self.paint)
+        self.c.bind('<B1-Motion>', self.visualize)
         self.c.bind('<ButtonRelease-1>', self.reset)
         
+    def toggle_mode(self):
+        if self.draw_mode == 'scribble':
+            self.draw_mode = 'line'
+        else:
+            self.draw_mode = 'scribble'
+    
     def paint(self, event):
-        self.line_width = self.DEFAULT_PEN_SIZE
-        paint_color = 'black'
-        if self.old_x and self.old_y:
-            self.c.create_line(self.old_x, self.old_y, event.x, event.y,
-                               width=self.line_width, fill=paint_color,
-                               capstyle='round', smooth=True)
-            
-        self.points.append((event.x, event.y))
         self.old_x = event.x
         self.old_y = event.y
-        
+    
+    def visualize(self, event):
+        if self.draw_mode == 'line':
+            if self.curr_line_obj is not None:
+                self.c.delete(self.curr_line_obj)
+            
+            if self.old_x and self.old_y:
+                self.curr_line_obj = self.c.create_line(self.old_x, self.old_y, event.x, event.y,
+                                width=self.line_width, fill='black',
+                                capstyle='round', smooth=True)
+                self.curr_line_x = event.x
+                self.curr_line_y = event.y
+        else:
+            if self.old_x and self.old_y:
+                self.c.create_line(self.old_x, self.old_y, event.x, event.y, 
+                                   width=self.DEFAULT_PEN_SIZE, fill='black',
+                                   capstyle='round', smooth=True)
+            self.points.append((event.x, event.y))
+            self.old_x = event.x
+            self.old_y = event.y
+        print("moving")
+    
     def reset(self, event):
+        if self.draw_mode == 'line':
+            distance = np.sqrt((self.curr_line_x - self.old_x)**2 + (self.curr_line_y - self.old_y)**2)
+            pixels_per_step = 2
+            line_x = np.linspace(self.old_x, self.curr_line_x, num=int(distance) // pixels_per_step)
+            line_y = np.linspace(self.old_y, self.curr_line_y, num=int(distance) // pixels_per_step)
+            self.points = np.c_[line_x, line_y]
+            self.curr_line_obj, self.curr_line_x, self.curr_line_y = None, None, None
+        
         self.old_x, self.old_y = None, None
         self.strokes.append(self.points)
         self.points = []
         
     def burn(self):
         print(f"strokes: {len(self.strokes)}")
-        angles = map_points_to_angles(self.strokes, self.CANVAS_WIDTH, self.CANVAS_HEIGHT, self.PHYSICAL_WIDTH, self.PHYSICAL_HEIGHT, 100)
-        write_angles_to_servo(angles)
+        
+        angles = self.writer.map_points_to_angles(self.strokes, self.CANVAS_WIDTH, self.CANVAS_HEIGHT, self.PHYSICAL_WIDTH, self.PHYSICAL_HEIGHT, 300)
+        try:
+            self.writer.write_angles_to_servo(angles)
+        except Exception as e:
+            self.writer.turn_laser_off()
+            raise e
+        print("##### DONE #####")
         
     def clear(self):
         self.c.delete('all')
---
+++ servo_writer.py
@@ -0,0 +1,76 @@
+import math
+from adafruit_servokit import ServoKit
+import time
+import board
+import busio
+import adafruit_pca9685
+
+
+class ServoWriter:
+    def __init__(self):
+        self.i2c = busio.I2C(board.SCL, board.SDA)
+        self.pca = adafruit_pca9685.PCA9685(self.i2c)
+        self.kit = ServoKit(channels=16)
+        self.pca.frequency = 60
+        
+        self.uptime = 0
+        self.max_uptime = 10
+        self.cooldown_period = 25
+        
+    def map_points_to_angles(self, strokes, canvas_width, canvas_height, physical_width, physical_height, distance_to_wall, angle_offset=90):
+        
+        angles = []
+        for stroke in strokes:
+            angle_stroke = []
+            for (x, y) in stroke:
+                
+                # convert absolute x, y to offset from center
+                center_offset_x = x - canvas_width / 2
+                center_offset_y = y - canvas_height / 2
+                
+                # convert pixel offset from the center to physical offset in mm
+                physical_offset_x = center_offset_x * physical_width / canvas_width
+                physical_offset_y = center_offset_y * physical_height / canvas_height
+                
+                # convert physical offset to angle
+                angle_x = math.atan(physical_offset_x / distance_to_wall)
+                angle_y = math.atan(physical_offset_y / distance_to_wall)
+                
+                # convert angle to servo angle
+                servo_angle_x = angle_offset + (angle_x * 180 / math.pi)
+                servo_angle_y = angle_offset - (angle_y * 180 / math.pi)
+                
+                angle_stroke.append((servo_angle_x, servo_angle_y))
+            angles.append(angle_stroke)
+
+        return angles
+        
+    def reset_position(self):
+        self.kit.servo[0].angle = 90
+        self.kit.servo[1].angle = 90
+                
+    def write_angles_to_servo(self, angles, point_interval=0.6):
+        self.reset_position()
+        
+        for stroke in angles:
+            self.turn_laser_on()
+            for i, (thx, thy) in enumerate(stroke):
+                print(f"writing angle ({thx}, {thy})")
+                self.kit.servo[0].angle = thx
+                self.kit.servo[1].angle = thy
+                time.sleep(point_interval)
+                self.uptime += point_interval
+                if self.uptime > self.max_uptime:
+                    self.turn_laser_off()
+                    time.sleep(self.cooldown_period)
+                    self.uptime =0
+                    self.turn_laser_on()
+                    
+            self.turn_laser_off()
+
+    def turn_laser_on(self, laser_channel = 2):
+        self.pca.channels[laser_channel].duty_cycle = 0xffff
+        
+    def turn_laser_off(self, laser_channel = 2):
+        self.pca.channels[laser_channel].duty_cycle = 0x0
+                
